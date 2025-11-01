cc269f5b3e6dfcc9c8f4f45ae9f520c5e4362313
Andrey Valinkin <valinkinandrey@gmail.com>
2025-11-02 00:51:03 +0300

    meower

--- src/main/java/ru/valinkin/meowapp/models/User.java
+++ src/main/java/ru/valinkin/meowapp/models/User.java
@@ -1,10 +1,9 @@
 package ru.valinkin.meowapp.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "users")
 public class User {
     @Id
     @Column(name = "ID")
---
+++ src/main/java/ru/valinkin/meowapp/services/AuthService.java
@@ -0,0 +1,8 @@
+package ru.valinkin.meowapp.services;
+
+public class AuthService {
+    public boolean Auth(String login, String password)
+    {
+        return true;
+    }
+}
