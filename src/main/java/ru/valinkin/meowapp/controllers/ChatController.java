af2b6fccc99f973e67a8108a2c51a1c1a52e5422
Andrey Valinkin <valinkinandrey@gmail.com>
2025-12-05 03:28:27 +0300

    chats and currentchat pages has merged

--- src/main/java/ru/valinkin/meowapp/controllers/ChatController.java
+++ src/main/java/ru/valinkin/meowapp/controllers/ChatController.java
@@ -1,30 +1,62 @@
 package ru.valinkin.meowapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import ru.valinkin.meowapp.models.User;
+import org.springframework.web.bind.annotation.*;
 import ru.valinkin.meowapp.security.PersonDetails;
 import ru.valinkin.meowapp.services.ChatService;
-import ru.valinkin.meowapp.services.UserDetailsService;
+import ru.valinkin.meowapp.services.MessageService;
+import ru.valinkin.meowapp.services.UserService;
+
+import java.util.Collections;
 
 @Controller
 public class ChatController {
+    private final MessageService messageService;
     private final ChatService chatService;
+    private final UserService userService;
 
-    ChatController(ChatService chatService){
+    @Autowired
+    public ChatController(MessageService messageService, ChatService chatService, UserService userService) {
+        this.messageService = messageService;
         this.chatService = chatService;
+        this.userService = userService;
     }
-    @GetMapping("/chats")
-    public String chatsPage(@ModelAttribute ("user") User user, Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
 
-        model.addAttribute("user", personDetails.getUser());
-        model.addAttribute("chats", chatService.getChats(personDetails.getUser().getID()));
+    @GetMapping("/chats")
+    public String ClearChat(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) auth.getPrincipal();
+
+
+        model.addAttribute("messages", Collections.emptyList());
+        model.addAttribute("chatId", 0);
+        model.addAttribute("currentUserId", personDetails.getUser().getID());
+        model.addAttribute("activeChats", chatService.getChats(personDetails.getUser().getID()));
+
+        return "chats_empty";
+    }
+
+    @GetMapping("/chats/{chatId}")
+    public String currentChat(@PathVariable int chatId, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) auth.getPrincipal();
+
+        model.addAttribute("messages", messageService.getMessages(chatId));
+        model.addAttribute("chatId", chatId);
+        model.addAttribute("currentUserId", personDetails.getUser().getID());
+        model.addAttribute("activeChats", chatService.getChats(personDetails.getUser().getID()));
+
         return "chats";
+    }
+
+    @PostMapping("/chats/{chatId}/send")
+    public String sendMessage(@PathVariable int chatId,
+                              @RequestParam String text) {
+        messageService.save(chatId, text);
+        return "redirect:/chats/" + chatId;
     }
 }
